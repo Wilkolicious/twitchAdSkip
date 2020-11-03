43419a49425278486f265645f4566bd060e13522
ToxiClay <ToxiClay@users.noreply.github.com>
2020-11-03 09:14:34 -0500

    making change safer and more complete
    Previous change did work, but it would have only caught that one specific case where maxRetries was exceeded. This code is more comprehensive and safer.

--- twitchAdSkip.js
+++ twitchAdSkip.js
@@ -31,14 +31,8 @@
   // Volume vals
   let videoNodeVolCurrent;
   let adLaunched = false;
-  
-  //dirty bool hack to fix "undefined" console message
-  let failedAfterMaxRetries = false;
 
   const log = function (logType, message) {
-    if (failedAfterMaxRetries) {
-      return;
-    }
     return console[logType](`${scriptName}: ${message}`);
   };
   const getFfzResetButton = function () {
@@ -176,7 +170,6 @@
       .catch((e) => {
         if (retries <= 0) {
           log('error', `${actionDescription} - failed after ${maxRetries} retries.`)
-          failedAfterMaxRetries = true;
           return Promise.reject(e);
         }
         log('warn', `${actionDescription} - retrying another ${retries} time(s).`);
@@ -225,7 +218,7 @@
       spawnVolumeChangeListener(videoPlayerEl);
       spawnVideoPlayerAdSkipObservers(videoPlayerEl);
     } catch (error) {
-      log('error', error);
+      log('error', error.message);
     }
   }
 
